ed0134ea65e7b19ded789df567db1b01ac38b90d
Richard Minsoo Go <67931944+RichardMinsooGo-ML@users.noreply.github.com>
2023-09-22 15:09:43 +0900

    Update and rename 14_A_mnist_TF2_Yolo_V3.py to C_4A_mnist_TF2_Yolo_V3.py

--- C_4A_mnist_TF2_Yolo_V3.py
+++ C_4A_mnist_TF2_Yolo_V3.py
@@ -149,7 +149,7 @@ Part B. Model Engineering
 '''
 
 '''
-1. Import Libraries
+01. Import Libraries
 '''
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
@@ -171,17 +171,88 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.regularizers import l2
 
 '''
-2. Hyperparameters
+02. Hyperparameters
 '''
 n_epoch = 10
 
 '''
-3. DropBlock
-Not used
+03. DropBlock
 '''
 
+from tensorflow.keras import backend as K
+
+keep_prob = 0.7
+
+def _bernoulli(shape, mean):
+    return tf.nn.relu(tf.sign(mean - tf.random.uniform(shape, minval=0, maxval=1, dtype=tf.float32)))
+
+
+class DropBlock2D(tf.keras.layers.Layer):
+    def __init__(self, keep_prob, block_size, scale=True,name=None, **kwargs):
+        super(DropBlock2D, self).__init__(name="DropBlock2D")
+        self.keep_prob = float(keep_prob) if isinstance(keep_prob, int) else keep_prob
+        self.block_size = int(block_size)
+        self.names = name
+        self.scale = tf.constant(scale, dtype=tf.bool) if isinstance(scale, bool) else scale
+        super(DropBlock2D, self).__init__(**kwargs)
+        
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update( {"block_size": self.block_size,"keep_prob": self.keep_prob,"name": self.names })
+
+        return config
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+    def build(self, input_shape):
+        assert len(input_shape) == 4
+        _, self.h, self.w, self.channel = input_shape.as_list()
+        # pad the mask
+        p1 = (self.block_size - 1) // 2
+        p0 = (self.block_size - 1) - p1
+        self.padding = [[0, 0], [p0, p1], [p0, p1], [0, 0]]
+        self.set_keep_prob()
+        super(DropBlock2D, self).build(input_shape)
+
+    def call(self, inputs, training=None, **kwargs):
+        def drop():
+            mask = self._create_mask(tf.shape(inputs))
+            output = inputs * mask
+            output = tf.cond(self.scale,
+                             true_fn=lambda: output *tf.cast(tf.size(mask), dtype=tf.float32)  / tf.reduce_sum(mask),
+                             false_fn=lambda: output)
+            return output
+
+        if training is None:
+            training = K.learning_phase()
+        output = tf.cond(tf.logical_or(tf.logical_not(training), tf.equal(self.keep_prob, 1.0)),
+                         true_fn=lambda: inputs,
+                         false_fn=drop)
+        return output
+
+    def set_keep_prob(self, keep_prob=None):
+        """This method only supports Eager Execution"""
+        if keep_prob is not None:
+            self.keep_prob = keep_prob
+        w, h = tf.cast(self.w, dtype=tf.float32), tf.cast(self.h, dtype=tf.float32)
+        
+        self.gamma = (1. - self.keep_prob) * (w * h) / (self.block_size ** 2) / \
+                     ((w - self.block_size + 1) * (h - self.block_size + 1))
+
+    def _create_mask(self, input_shape):
+        sampling_mask_shape = tf.stack([input_shape[0],
+                                       self.h - self.block_size + 1,
+                                       self.w - self.block_size + 1,
+                                       self.channel])
+        mask = _bernoulli(sampling_mask_shape, self.gamma)
+        mask = tf.pad(mask, self.padding)
+        mask = tf.nn.max_pool(mask, [1, self.block_size, self.block_size, 1], [1, 1, 1, 1], 'SAME')
+        mask = 1 - mask
+        return mask
+
 '''
-4. Convolutional
+04. Leaky Convolutional
 '''
 def compose(*funcs):
     # return lambda x: reduce(lambda v, f: f(v), funcs, x)
@@ -192,7 +263,7 @@ def compose(*funcs):
 
 @wraps(Conv2D)
 def DarknetConv2D(*args, **kwargs):
-    """Wrapper to set Darknet parameters for Convolution2D."""
+    '''Wrapper to set Darknet parameters for Convolution2D.'''
     darknet_conv_kwargs = {'kernel_initializer': 'he_normal'}
     if kwargs.get('strides') == (2, 2):
         darknet_conv_kwargs['padding'] = 'valid'
@@ -202,7 +273,7 @@ def DarknetConv2D(*args, **kwargs):
     return Conv2D(*args, **darknet_conv_kwargs)
 
 def DarknetConv2D_BN_Leaky(*args, **kwargs):
-    """Darknet Convolution2D followed by BatchNormalization and LeakyReLU."""
+    '''Darknet Convolution2D followed by BatchNormalization and LeakyReLU.'''
     no_bias_kwargs = {'use_bias': False}
     no_bias_kwargs.update(kwargs)
     return compose(
@@ -211,22 +282,32 @@ def DarknetConv2D_BN_Leaky(*args, **kwargs):
         LeakyReLU(alpha=0.1))
 
 '''
-5. Residual Block
+05. [Not Used] Mish Activation
+'''
+
+'''
+06. [Not Used] Mish Convolutional
+'''
+
+'''
+07. Residual Block
 '''
 def resblock_body(x, num_filters, num_blocks):
     '''A series of resblocks starting with a downsampling Convolution2D'''
     # Darknet uses left and top padding instead of 'same' mode
     x = ZeroPadding2D(((1, 0), (1, 0)))(x)
     x = DarknetConv2D_BN_Leaky(num_filters, (3, 3), strides=(2, 2))(x)
+    x = DropBlock2D(keep_prob=keep_prob, block_size=3)(x)
     for _ in range(num_blocks):
         y = compose(
             DarknetConv2D_BN_Leaky(num_filters//2, (1, 1)),
+            DropBlock2D(keep_prob=keep_prob, block_size=3),
             DarknetConv2D_BN_Leaky(num_filters, (3, 3)))(x)
         x = Add()([x, y])
     return x
 
 '''
-6. Backbone
+08. Backbone darknet
 '''
 def Backbone_darknet(input_tensor):
     '''Darknent body having 52 Convolution2D layers'''
@@ -239,31 +320,37 @@ def Backbone_darknet(input_tensor):
     return x
 
 
-'''
-7. Neck
-'''
 def make_last_layers(x, num_filters):
     '''6 Conv2D_BN_Leaky layers followed by a Conv2D_linear layer'''
     x = compose(
             DarknetConv2D_BN_Leaky(num_filters, (1, 1)),
+            DropBlock2D(keep_prob=keep_prob, block_size=3),
             DarknetConv2D_BN_Leaky(num_filters*2, (3, 3)),
+            DropBlock2D(keep_prob=keep_prob, block_size=3),
             DarknetConv2D_BN_Leaky(num_filters, (1, 1)),
+            DropBlock2D(keep_prob=keep_prob, block_size=3),
             DarknetConv2D_BN_Leaky(num_filters*2, (3, 3)),
+            DropBlock2D(keep_prob=keep_prob, block_size=3),
             DarknetConv2D_BN_Leaky(num_filters, (1, 1)))(x)
     y = DarknetConv2D_BN_Leaky(num_filters*2, (3, 3))(x)
     return x, y
 
+'''
+09. [Not Used] SPP Module
+'''
 WEIGHTS_PATH_DN_BODY    = "https://github.com/samson6460/tf2_YOLO/releases/download/1.0/tf_keras_yolov3_body.h5"
 WEIGHTS_PATH_DN53_TOP   = "https://github.com/samson6460/tf2_YOLO/releases/download/Weights/tf_keras_darknet53_448_include_top.h5"
 WEIGHTS_PATH_DN53_NOTOP = "https://github.com/samson6460/tf2_YOLO/releases/download/Weights/tf_keras_darknet53_448_no_top.h5"
 
+'''
+10. YOLO Neck
+'''
 def yolo_neck(input_shape=(416, 416, 3),
               pretrained_darknet=None,
               pretrained_weights=None):
-    """Create YOLO_V3 model CNN body in Keras."""
+    '''Create YOLO_V3 model CNN body in Keras.'''
     inputs = Input(input_shape)
     darknet = Model(inputs, Backbone_darknet(inputs))
-    
     if pretrained_darknet is not None:
         darknet.set_weights(pretrained_darknet.get_weights())
     
@@ -282,6 +369,7 @@ def yolo_neck(input_shape=(416, 416, 3),
     x, y3 = make_last_layers(x, 128)
     model = Model(inputs, [y1, y2, y3])
     
+    '''
     if pretrained_weights is not None:
         if pretrained_weights == "pascal_voc":
             pretrained_weights = get_file(
@@ -289,11 +377,11 @@ def yolo_neck(input_shape=(416, 416, 3),
                 WEIGHTS_PATH_DN_BODY,
                 cache_subdir="models")
         model.load_weights(pretrained_weights)
-    
+    ''' 
     return model
 
 '''
-8. Head
+11. Head
 '''
 def yolo_head(model_body, class_num=10, 
               anchors=[[0.89663461, 0.78365384],
@@ -348,7 +436,7 @@ import numpy as np
 epsilon = 1e-07
 
 '''
-9. Intersection over Union
+12. Intersection over Union
 '''
 def cal_iou(xywh_true, xywh_pred, grid_shape):
     grid_shape = np.array(grid_shape[::-1])
@@ -380,7 +468,7 @@ def cal_iou(xywh_true, xywh_pred, grid_shape):
     return iou_scores
 
 '''
-10. Yolo Loss Function
+13. Yolo Loss Function
 '''
 def wrap_yolo_loss(grid_shape,
                    bbox_num,
@@ -404,7 +492,7 @@ def wrap_yolo_loss(grid_shape,
         xywh_pred = y_pred[..., :4] # N*S*S*B*4
 
         iou_scores = cal_iou(xywh_true, xywh_pred, grid_shape) # N*S*S*B
-
+        
         response_mask = tf.one_hot(tf.argmax(iou_scores, axis=-1),
                                    depth=bbox_num,
                                    dtype=xywh_true.dtype) # N*S*S*B
@@ -482,7 +570,7 @@ def wrap_yolo_loss(grid_shape,
     return yolo_loss
 
 '''
-11. [Opt] Define Custom Metrics 1
+14. [Opt] Define Custom Metrics 1
 Object Accuracy
 '''
 from tensorflow.keras.metrics import binary_accuracy
@@ -509,7 +597,7 @@ def wrap_obj_acc(grid_shape, bbox_num, class_num):
     return obj_acc
 
 '''
-12. [Opt] Define Custom Metrics 2
+15. [Opt] Define Custom Metrics 2
 Mean IOU
 '''
 def wrap_mean_iou(grid_shape, bbox_num, class_num):
@@ -536,7 +624,7 @@ def wrap_mean_iou(grid_shape, bbox_num, class_num):
     return mean_iou
 
 '''
-13. [Opt] Define Custom Metrics 3
+16. [Opt] Define Custom Metrics 3
 Class Accuracy
 '''
 def wrap_class_acc(grid_shape, bbox_num, class_num):
@@ -565,7 +653,7 @@ def wrap_class_acc(grid_shape, bbox_num, class_num):
     return class_acc
 
 '''
-14. Build Class for Model, Loss, Metrics
+17. Build Class for Model, Loss, Metrics
 '''
 class Yolo(object):
 
@@ -583,7 +671,7 @@ class Yolo(object):
         self.file_names = None
         
     '''
-    15. Model Create
+    18. Model Create
     '''
     def create_model(self,
                      anchors=[[0.89663461, 0.78365384],
@@ -622,7 +710,7 @@ class Yolo(object):
         self.abox_num = len(self.anchors)//self.fpn_layers
 
     '''
-    16. Loss Create
+    19. Loss Create
     '''
     def loss(self,
              binary_weight=1,
@@ -659,7 +747,7 @@ class Yolo(object):
         return loss_list
     
     '''
-    17. Metrics Create
+    20. Metrics Create
     '''
     def metrics(self, type="obj_acc"):
         
@@ -694,9 +782,8 @@ class Yolo(object):
 yolo = Yolo(class_names=class_names)
 
 '''
-18. Get anchor boxes
+21. Get anchor boxes
 '''
-
 from utils.kmeans import kmeans, iou_dist, euclidean_dist
 import numpy as np
 
@@ -719,8 +806,7 @@ plt.scatter(anchors[..., 0],
 plt.show()
 
 '''
-19. Build NN model
-from class
+22. Build NN model from class
 '''
 anchors=[[0.26923078, 0.26923078],
          [0.20192307, 0.20192307],
@@ -736,8 +822,15 @@ yolo.create_model(anchors=anchors)
 yolo.model.summary()
 
 '''
+23. Define Optimizer
+'''
+from tensorflow.keras.optimizers import SGD, Adam
+
+optimizer = Adam(learning_rate=5e-5)
+
+'''
 Callback function
-20. Learning Rate Scheduling
+24. Learning Rate Scheduling
 '''
 from tensorflow.keras.callbacks import LearningRateScheduler
 
@@ -752,15 +845,7 @@ def scheduler(epoch, lr):
 callback = LearningRateScheduler(scheduler)
 
 '''
-21. Define Optimizer
-'''
-from tensorflow.keras.optimizers import SGD, Adam
-
-optimizer = Adam(learning_rate=5e-5)
-
-'''
-22. Loss Function
-from YOLO class
+25. Loss Function from YOLO class
 '''
 from utils.tools import get_class_weight
 
@@ -794,13 +879,13 @@ loss_fn = yolo.loss(
     ignore_thresh=ignore_thresh
     )
 '''
-23. Build Metrics
+26. Build Metrics
 from Yolo Class
 '''
 metrics = yolo.metrics("obj+iou+class")
 
 '''
-24. Model Compilation
+27. Model Compilation
 '''
 yolo.model.compile(
     optimizer = optimizer,
@@ -813,7 +898,7 @@ import time
 start_time = time.time()
 
 '''
-25. Model Training and Validation
+28. Model Training and Validation
 '''
 train_history = yolo.model.fit(
     train_img,
@@ -826,7 +911,7 @@ train_history = yolo.model.fit(
     )
 
 '''
-26. Predict and evaluate
+29. Predict and evaluate
 '''
 prediction = yolo.model.predict(test_img, batch_size=10)
 
@@ -845,9 +930,8 @@ finish_time = time.time()
 print(int(finish_time - start_time), "Sec")
 
 '''
-26. Show score table
+30. Show score table
 '''
-
 from utils.measurement import create_score_mat
 
 create_score_mat(
